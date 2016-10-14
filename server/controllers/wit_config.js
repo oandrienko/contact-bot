e49fd2afe0abb73d451e223104bfe3835eb0da5b
Oles Andrienko <andrienko@live.ca>
2016-10-13 22:51:46 -0400

    Fix naming convention

--- server/controllers/wit_config.js
+++ server/controllers/wit_config.js
@@ -4,7 +4,7 @@ const TOKEN = process.env.BOT_TOKEN;
 
 export default function(actions) {
 	return new Wit({
-	  accessToken: TOKEN,
+	  accessToken: 'L3JVFQEOLLQQAI3SVPZH4PNOV6CJ6WVL',
 	  logger: new log.Logger(log.INFO),
 	  actions
 	});
